ef9ae8194004ea96c580449d491a552cdf2f4039
Mostafa Hosseini <54173709+mostafa1350@users.noreply.github.com>
2025-03-12 00:30:33 +0330

    copy hex code to textArea

--- myJS.js
+++ myJS.js
@@ -1,12 +1,17 @@
 const colorInputs = document.querySelectorAll(".colors input");
 const gradientBox = document.querySelector(".gradienBox");
 const selectMenu = document.querySelector(".select-box select");
+const textArea = document.querySelector("textarea");
+const btnReferesh = document.querySelector(".refresh");
+
+
 
 const generateGradient =()=>{
     const gradient = `linear-gradient(${selectMenu.value},
                     ${colorInputs[0].value},${colorInputs[1].value})`;
 
     gradientBox.style.background = gradient;
+    textArea.value = `background: ${gradient}`;
 
 }
 
